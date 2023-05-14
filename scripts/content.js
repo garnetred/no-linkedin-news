556473e6f673bba6151cb943f29c92b6afded511
December G <dgarnetsmith@gmail.com>
2023-05-14 16:42:13 -0500

    Fix issue with home page news section not being hidden after navigating to other page, introduce issue with news page not being hidden on page reload

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,10 @@
 let currentPage;
 let currentResponse;
 
-(() => {
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { page } = obj;
-    if (page && response) {
-      currentPage = page;
-      currentResponse = response;
-      //   hideNews();
-    }
-  });
-})();
-
-// const newsModuleNode = document.getElementById('feed-news-module');
-
 const callback = () => {
   const newsModuleElement = document.querySelector('#feed-news-module');
   const newsPageElement = document.querySelector('.scaffold-layout');
+
   if (newsModuleElement && currentPage === 'feed') {
     // hideNews();
     console.log('on home page');
@@ -30,21 +18,39 @@ const callback = () => {
 };
 const observer = new MutationObserver(callback);
 
-const config = { childList: true, subtree: true };
+const config = { attributes: true, childList: true, subtree: true };
 
-observer.observe(document, config);
+(() => {
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { page } = obj;
+    if (page && response) {
+      currentPage = page;
+      currentResponse = response;
+      observer.observe(document, config);
+      //   hideNews();
+    }
+  });
+})();
+
+console.log(currentPage, 'current page');
+
+// if (currentPage && (currentPage === 'feed' || currentPage === 'news')) {
+//   observer.observe(document, config);
+// } else if (!currentPage) {
+//   observer.observe(document, config);
+// }
 
 // observer.disconnect();
 
-const hideNews = () => {
-  console.log(currentPage, currentResponse);
-  console.log();
-  if (currentResponse && currentPage === 'feed') {
-    let newsFeedModule = document.getElementById('feed-news-module');
-    console.log(newsFeedModule, 'news feed module?');
-    newsFeedModule.classList.add('hidden');
-  } else if (currentResponse && currentPage === 'news') {
-    let newsFeedPage = document.getElementsByClassName('scaffold-layout')[0];
-    newsFeedPage.classList.add('hidden');
-  }
-};
+// const hideNews = () => {
+//   console.log(currentPage, currentResponse);
+//   console.log();
+//   if (currentResponse && currentPage === 'feed') {
+//     let newsFeedModule = document.getElementById('feed-news-module');
+//     console.log(newsFeedModule, 'news feed module?');
+//     newsFeedModule.classList.add('hidden');
+//   } else if (currentResponse && currentPage === 'news') {
+//     let newsFeedPage = document.getElementsByClassName('scaffold-layout')[0];
+//     newsFeedPage.classList.add('hidden');
+//   }
+// };
