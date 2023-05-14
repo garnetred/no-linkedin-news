5505076ae6be2bb5ca81768177e6b0f2f11b8aca
December G <dgarnetsmith@gmail.com>
2023-05-14 15:32:12 -0500

    Save working changes before implementing mutation observer

--- scripts/content.js
+++ scripts/content.js
@@ -3,36 +3,27 @@ let currentResponse;
 
 const hideNews = () => {
   console.log('loaded!');
+  console.log({ currentResponse, currentPage });
   if (currentResponse && currentPage === 'feed') {
-    let newsFeedModule = document.querySelector('#feed-news-module');
+    let newsFeedModule = document.getElementById('feed-news-module');
     console.log(newsFeedModule, 'news feed module?');
     newsFeedModule.classList.add('hidden');
   } else if (currentResponse && currentPage === 'news') {
-    let newsFeedPage = document.querySelector('.scaffold-layout');
+    let newsFeedPage = document.getElementsByClassName('scaffold-layout')[0];
     newsFeedPage.classList.add('hidden');
   } else {
-    console.error(response.error);
+    console.error(currentResponse.error);
   }
 };
-
-document.addEventListener('DOMContentLoaded', hideNews);
-// could store both page and response at a high level
-// only after the page is loaded invoke hideNews?
-// separate function that checks if there's been a change to page and response?
-
-const checkForUpdates = () => {
-
-}
 
 (() => {
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { page } = obj;
     // hideNews(page, response);
     if (page && response) {
-        currentPage = page;
-        currentResponse = response;
+      currentPage = page;
+      currentResponse = response;
+      hideNews();
     }
-
   });
 })();
-
