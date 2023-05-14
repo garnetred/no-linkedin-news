21fdf61089867c4310e5da3e557981194fda9888
December G <dgarnetsmith@gmail.com>
2023-05-13 22:40:33 -0500

    Fix formatting

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,16 @@
 const hideNews = (page, response) => {
-    console.log("loaded!")
+  console.log('loaded!');
 
-    if (response && page === 'feed') {
-        let newsFeedModule = document.querySelector('#feed-news-module');
-       console.log(newsFeedModule, 'news feed module?')
-       newsFeedModule.classList.add('hidden');
-
-    } else if (response && page === 'news') {
-                      let newsFeedPage = document.querySelector('.scaffold-layout');
-            newsFeedPage.classList.add('hidden');
-            // newsFeedModule = undefined;
-    }
-
-}
+  if (response && page === 'feed') {
+    let newsFeedModule = document.querySelector('#feed-news-module');
+    console.log(newsFeedModule, 'news feed module?');
+    newsFeedModule.classList.add('hidden');
+  } else if (response && page === 'news') {
+    let newsFeedPage = document.querySelector('.scaffold-layout');
+    newsFeedPage.classList.add('hidden');
+    // newsFeedModule = undefined;
+  }
+};
 
 // let hasUrl = false;
 // let page;
@@ -21,21 +19,16 @@ const hideNews = (page, response) => {
 document.addEventListener('DOMContentLoaded', loadPage, false);
 
 const loadPage = () => {
-    return true;
-    // what should this function do?
-}
+  return true;
+  // what should this function do?
+};
 
 (async () => {
-
-    // let newsFeedModule;
-    // let newsFeedPage;
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-         const {page} = obj;
-         console.log('message received', page)
-        hideNews(page, response)
-
-     })
-
-})()
-
-
+  // let newsFeedModule;
+  // let newsFeedPage;
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { page } = obj;
+    console.log('message received', page);
+    hideNews(page, response);
+  });
+})();
