0b8c955b512edd2b482ca2a041f739b117648b25
December G <dgarnetsmith@gmail.com>
2023-05-14 19:29:53 -0500

    Delete content.js and unused code, comments, console logs

--- scripts/background.js
+++ scripts/background.js
@@ -5,11 +5,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     tabUrl &&
     tabUrl.includes('linkedin.com/feed')
   ) {
-    chrome.tabs.sendMessage(tabId, { page: 'feed', tabId });
-    // chrome.tabs.insertCSS({
-    //   file: 'css/newsfeed.css',
-    //   run_at: 'document_start',
-    // });
     chrome.scripting.insertCSS({
       target: { tabId: tabId },
       files: ['css/feed.css'],
@@ -19,16 +14,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     tabUrl &&
     tabUrl.includes('linkedin.com/news')
   ) {
-    chrome.tabs.sendMessage(tabId, { page: 'news', tabId });
     chrome.scripting.insertCSS({
       target: { tabId: tabId },
       files: ['css/news.css'],
     });
   }
 });
-
-// function sendMessageToActiveTab(message) {
-//   const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-//   const response = await chrome.tabs.sendMessage(tab.id, message);
-//   // TODO: Do something with the response.
-// }
